0b528f707a469a42089c50148d128c87398e36bc
Ayşegül Azak <2110206031@ogrenci.karabuk.edu.tr>
2024-09-25 18:22:24 +0300

    Add project files.

---
+++ Week_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+
+            double r;
+            Console.WriteLine("Enter Radius: ");
+            r= Convert.ToDouble(Console)
+
+        }
+    }
+}
